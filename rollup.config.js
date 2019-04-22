7b36d09941f753f6af58aaeb68a3e6158f8832eb
Adrian de la Rosa <adrian@delarosab.me>
2019-04-22 10:43:38 +0200

    :heavy_plus_sign: Replacing external dependency

--- rollup.config.js
+++ rollup.config.js
@@ -1,9 +1,9 @@
+import external from 'rollup-plugin-auto-external';
 import babel from 'rollup-plugin-babel';
 import minify from 'rollup-plugin-babel-minify';
 import commonjs from 'rollup-plugin-commonjs';
 import del from 'rollup-plugin-delete';
 import resolve from 'rollup-plugin-node-resolve';
-import external from 'rollup-plugin-peer-deps-external';
 import replace from 'rollup-plugin-replace';
 import { sizeSnapshot } from 'rollup-plugin-size-snapshot';
 import visualizer from 'rollup-plugin-visualizer';
@@ -29,7 +29,7 @@ const config = {
     },
   ],
   plugins: [
-    external({ includeDependencies: true }),
+    external(),
     del({
       targets: ['./dist/'],
     }),
