b51ec4838d55dd1451a620d06f9efd813c676ad0
Adrian de la Rosa <adrian@delarosab.me>
2019-06-18 11:31:59 +0200

    :bug: Fixing wrong path

--- index.module.js
+++ index.module.js
@@ -1,5 +1,5 @@
 if (process.env.NODE_ENV === 'production') {
-  module.exports = require('./es/calvin.production.min.js');
+  module.exports = require('./dist/es/calvin.production.min.js');
 } else {
-  module.exports = require('./es/calvin.development.js');
+  module.exports = require('./dist/es/calvin.development.js');
 }
