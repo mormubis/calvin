5ef55e446fc2324e97bd6022760dde869eb080f3
Adrian de la Rosa <adrian@delarosab.me>
2019-06-19 16:05:26 +0200

    :ambulance: Fixing dist path

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 if (process.env.NODE_ENV === 'production') {
-  module.exports = require('./dist/cjs/calvin.production.min.js');
+  module.exports = require('./dist/cjs/calvin.production.js');
 } else {
   module.exports = require('./dist/cjs/calvin.development.js');
 }
